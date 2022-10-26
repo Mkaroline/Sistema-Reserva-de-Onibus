0f148074160dae6973ea9bc79cc70ae76233821a
maria karoline <112703319+Mkaroline@users.noreply.github.com>
2022-10-26 10:14:47 -0300

    alterar

--- reserva.c
+++ reserva.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "reserva.h"
-struct reserva_de_onibus{
+
+struct reserva_de_onibus{//estrutura reserva de onubus onde tem o codigo
+	//vagas, origem, destino, passagem do cliente;
 	int codigo;
 	int vagas;
 	char origem[100];
 	char destino[100];
-	char passagens[100];
+	char passagem[100];
 };
 struct no{
 	reserva_de_onibus dado;
@@ -14,10 +16,10 @@ struct no{
 	};
 	struct lista{
 		no* inicio;
-	
-}
+};
 
-void tabela(){
+void tabela_reserva_de_onibus(){
+	
 	printf("       ESCOLHA A OPCAO   \n");
 	printf("======================================\n");
 	printf("  1- realizar reservas\t\n");
@@ -27,11 +29,11 @@ void tabela(){
     printf(" 5- editar reservas\t\n");
  	printf(" 6- consultar vagas disponiveis para uma dada origem - destino\t\n");
     printf(" 7- consultar quantitativo de passageiros\t\n");
-	printf(" 8- sair\t\n");
-	
+	printf(" 8- sair\t\n");	
 }
 
 void reserva(reserva_de_onibus dado *r){
+
 	printf("codigo do cliente: ");
 	scanf("%d",&r-> codigo);
 	printf("A vaga que deseja: ");
@@ -43,7 +45,30 @@ void reserva(reserva_de_onibus dado *r){
     printf("A passagem: ");
 	scanf(" %[^\n]", r->pasagem);
 }
+int excluir_reserva(reserva_de_onibus dado *r){	
+	 if (r == NULL){
+	return 1;
+  }else {
+	return 0;
+  }
+}
 
+void listar_reservas(reserva_de_onibus dado *r){
+}
+void buscar_reservas(reserva_de_onibus dado *r){
+	No*busca(Lista lista, int cod){
+	for (p1 = p -> inici; p1 != NULL && p1-> dado.codigo != cod; p1 = p1->NULL){
+		return p1;
+	}
+}
+}
+void editar_reservas(reserva_de_onibus dado *r){
+}
+void consultar_vagas_disponiveis(reserva_de_onibus dado *r){
+}
+void consultar_quantitativo_de_passageiros(reserva_de_onibus dado *r){
+}
+/*
 void inserir_inicio(Listas *p,reserva_de_onibus dado){//serve para modificar a lista no inicio
 	No* novo = (no*)malloc(sizeof(no));
 	novo-> dado =dado;
@@ -171,4 +196,4 @@ No*busca(Lista lista, int cod){
 	for (p1 = p -> inici; p1 != NULL && p1-> dado.codigo != cod; p1 = p1->NULL){
 		return p1;
 	}
-}
+}*/
