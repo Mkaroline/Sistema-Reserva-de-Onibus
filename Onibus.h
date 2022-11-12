2498548d1f7f38f5a32547de45b136829249b45b
maria karoline <mariakarolineminodemouram@gmail.com>
2022-11-12 14:53:13 -0300

    karol aqui de novo

--- Onibus.h
+++ Onibus.h
@@ -6,7 +6,6 @@ Lista* lst_cria(void);
 Lista* l_insere(Lista* l);//lista para insire 
 int lst_vazia(Lista *l);//função que cria uma lista vazia (NULL)
 Lista* excluir(Lista* l, char* destino);
-void l_listar(Lista*l);
-Lista * l_busca(int elem, Lista* l);
+
 
  
